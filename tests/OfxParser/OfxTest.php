d523fd66db55e97b8ef885e66b0fc0cbe2b402bb
Ambroise Maupate <ambroise.maupate@gmail.com>
2016-08-17 20:53:36 +0200

    Fixed test wrong Ofx constructor usage.

--- tests/OfxParser/OfxTest.php
+++ tests/OfxParser/OfxTest.php
@@ -19,9 +19,12 @@ class OfxTest extends \PHPUnit_Framework_TestCase
         $this->ofxdata = simplexml_load_string( file_get_contents($ofxFile) );
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     * @expectedExceptionMessage Argument 1 passed to OfxParser\Ofx::__construct() must be an instance of SimpleXMLElement, string given
+     */
     public function testAcceptOnlySimpleXMLElement()
     {
-        $this->setExpectedException('\TypeError');
         new Ofx('This is not an SimpleXMLObject');
     }
 
