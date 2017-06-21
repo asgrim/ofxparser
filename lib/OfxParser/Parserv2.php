ca3ccee699564db2dfbee17e471d428076eeafdb
Jacques Marneweck <jacques@powertrip.co.za>
2017-06-21 23:29:40 +0000

    Parse the 2.00 format of the OFX file

---
+++ lib/OfxParser/Parserv2.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace OfxParser;
+
+/**
+ * An OFX parser library
+ *
+ * Heavily refactored from Guillaume Bailleul's grimfor/ofxparser
+ *
+ * @author Guillaume BAILLEUL <contact@guillaume-bailleul.fr>
+ * @author James Titcumb <hello@jamestitcumb.com>
+ * @author Oliver Lowe <mrtriangle@gmail.com>
+ */
+class Parserv2
+{
+    /**
+     * Load an OFX file into this parser by way of a filename
+     *
+     * @param string $ofxFile A path that can be loaded with file_get_contents
+     * @return Ofx
+     * @throws \Exception
+     */
+    public function loadFromFile($ofxFile)
+    {
+        if (!file_exists($ofxFile)) {
+            throw new \InvalidArgumentException("File '{$ofxFile}' could not be found");
+        }
+
+        return $this->loadFromString(file_get_contents($ofxFile));
+    }
+
+    /**
+     * Load an OFX by directly using the text content
+     *
+     * @param string $ofxContent
+     * @return  Ofx
+     * @throws \Exception
+     */
+    public function loadFromString($ofxContent)
+    {
+        $ofxContent = utf8_encode($ofxContent);
+        $xml = $this->xmlLoadString($ofxContent);
+
+        return new Ofx($xml);
+    }
+
+    /**
+     * Detect if the OFX file is on one line. If it is, add newlines automatically.
+     *
+     * @param string $ofxContent
+     * @return string
+     */
+    private function conditionallyAddNewlines($ofxContent)
+    {
+        if (preg_match('/<OFX>.*<\/OFX>/', $ofxContent) === 1) {
+            return str_replace('<', "\n<", $ofxContent); // add line breaks to allow XML to parse
+        }
+
+        if (preg_match('/<.+>.*<.+>/', $ofxContent) === 1) {
+            return str_replace('<', "\n<", $ofxContent); // add line breaks to allow XML to parse
+        }
+
+        return $ofxContent;
+    }
+
+    /**
+     * Load an XML string without PHP errors - throws exception instead
+     *
+     * @param string $xmlString
+     * @throws \Exception
+     * @return \SimpleXMLElement
+     */
+    private function xmlLoadString($xmlString)
+    {
+        libxml_clear_errors();
+        libxml_use_internal_errors(true);
+        $xml = simplexml_load_string($xmlString);
+
+        if ($errors = libxml_get_errors()) {
+            throw new \RuntimeException('Failed to parse OFX: ' . var_export($errors, true));
+        }
+
+        return $xml;
+    }
+
+    /**
+     * Detect any unclosed XML tags - if they exist, close them
+     *
+     * @param string $line
+     * @return string
+     */
+    private function closeUnclosedXmlTags($line)
+    {
+        // Matches: <SOMETHING>blah
+        // Does not match: <SOMETHING>
+        // Does not match: <SOMETHING>blah</SOMETHING>
+        if (preg_match(
+            "/<([A-Za-z0-9.]+)>([\wà-úÀ-Ú0-9\.\-\_\+\, ;:\[\]\'\&\/\\\*\(\)\+\{\|\}\!\£\$\?=@€£#%±§~`]+)$/",
+            trim($line),
+            $matches
+        )) {
+            return "<{$matches[1]}>{$matches[2]}</{$matches[1]}>";
+        }
+        return $line;
+    }
+
+    /**
+     * Convert an SGML to an XML string
+     *
+     * @param string $sgml
+     * @return string
+     */
+    private function convertSgmlToXml($sgml)
+    {
+        $sgml = str_replace(["\r\n", "\r"], "\n", $sgml);
+
+        $lines = explode("\n", $sgml);
+
+        $xml = '';
+        foreach ($lines as $line) {
+            $xml .= trim($this->closeUnclosedXmlTags($line)) . "\n";
+        }
+
+        return trim($xml);
+    }
+}
